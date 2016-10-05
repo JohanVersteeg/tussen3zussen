72079949e22145a5422c6f9ab299f151808e2dae
PC-JOHAN\Johan <johan.versteeg@hroffice.nl>
2016-10-05 13:01:22 +0200

    Updated instagram feed

--- instagram.php
+++ instagram.php
@@ -2,11 +2,31 @@
     <div class="row">
         <div class="title left"></div>
         <div class="photoroll">
-            <?php for($i = 1; $i < 7; $i++) { ?>
+
+<?php
+    $json = file_get_contents("https://www.instagram.com/girlonthewhitebike/media/");
+    $media = json_decode($json);
+?>
+
+            <?php 
+                for($i = 0; $i < 6; $i++) {
+                    $mediaItem = $media->items[$i];
+            ?>
                 
                 <div class="col-sm-2" >
-                    <img src="<?php bloginfo('template_directory');?>/images/instagram/<?php echo $i; ?>.jpg" />
-                    <div class="shadow"></div>
+                    <img src="<?php echo $mediaItem->images->low_resolution->url ?>" />
+                    <div class="shadow" onclick="document.location.href = '<?php echo $mediaItem->link; ?>';">
+                        <span class="media-information">
+                            <span class="likes">
+                                <i class="fa fa-heart" aria-hidden="true"></i>
+                                <span class="number"> <?php echo $mediaItem->likes->count; ?> </span>
+                            </span>
+                            <span class="comments">
+                                <i class="fa fa-comment" aria-hidden="true"></i>
+                                <span class="number"> <?php echo $mediaItem->comments->count; ?> </span>
+                            </span>
+                        </span>
+                    </div>
                 </div>
             <?php } ?>    
         </div>
