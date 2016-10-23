0df43f716ac01d0cc394a2a4de45f5c774f854b9
PC-JOHAN\Johan <johan.versteeg@hroffice.nl>
2016-10-23 19:18:56 +0200

    Final version 1.0.0  - Show text and images before read more text again  - Enabled CI

--- content-single-post.php
+++ content-single-post.php
@@ -2,7 +2,7 @@
 	<h2 class="blog-post-title"><?php the_title(); ?></h2>
 	<p class="blog-post-meta"><?php the_date(); ?></p>
 
- 	<?php the_content(null, true); ?>
+ 	<?php the_content(null, false); ?>
 	<?php if(!is_page()){ ?>
 		<small>Geplaats in categorie <?php the_category(', ') ?> met tags <?php the_tags("",', ') ?></small>
 	<?php } ?>
